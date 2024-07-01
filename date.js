71f2c1b609bf3f511475a7d65235844a8763a697
said-ctrl <fifinabs@gmail.com>
2024-07-01 16:38:04 +0200

    update

---
+++ date.js
@@ -0,0 +1,30 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let datuf = [];
+    fetch('./json/datesufc.json').then(res => res.json()).then(data => {
+        for (let i = 0; i < data.length; i++) {
+            datuf.push({ title: data[i].Name, start: data[i].DateTime, }
+            )
+        }
+
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+        events:datuf,
+        });
+        calendar.render();
+
+
+    })
+
+
+
+
+
+
+
+
+})
+
--- script.js
+++ script.js
@@ -4,45 +4,41 @@ async function affiche() {
     const fox = await reponse.json();
     new gridjs.Grid({
         columns: [{
-            id: 'Nickname',
-            name: 'Pseudo',
-        },
-        {
-            id: 'FirstName',
-            name: 'Prenom',
-        },
-        {
-            id: 'LastName',
-            name: 'Nom',
-        },
-        {
-            id: 'Wins',
-            name: 'Victoires',
+                id: 'FirstName',
+                name: 'Prenom',
+            },
+            {
+                id: 'LastName',
+                name: 'Nom',
+            },
+            {
+                id: 'Wins',
+                name: 'Victoires',
 
-        },
-        {
-            id: 'Losses',
-            name: 'Defaites'
-        },
-        {
-            id: 'BirthDate',
-            name: 'Naissance',
-            formatter: (cell) => ` ${cell.replace('T00:00:00', '')}`
-        },
-        {
-            id: 'CareerStats',
-           
-        
-        
-            name: 'Actions',
-            formatter: (cell, row) => {
-                return gridjs.h('button', {
-                    className: 'py-2 mb-4 px-4 border rounded-md text-white bg-blue-600',
-                    onClick: () => alert(`Editing "${row.cells[0].data}" "${row.cells[1].data}"`)
-                }, 'Edit');
-            }
-        },
-     ],
+            },
+            {
+                id: 'Losses',
+                name: 'Defaites'
+            },
+            {
+                id: 'BirthDate',
+                name: 'Naissance',
+                formatter: (cell) => ` ${cell.replace('T00:00:00', '')}`
+            },
+            {
+                id: 'CareerStats',
+
+
+
+                name: 'Actions',
+                formatter: (cell, row) => {
+                    return gridjs.h('button', {
+                        className: 'py-2 mb-4 px-4 border rounded-md text-white bg-blue-600',
+                        onClick: () => alert(`Editing "${row.cells[0].data}" "${row.cells[1].data}"`)
+                    }, 'Edit');
+                }
+            },
+        ],
         search: true,
         data: fox,
         sort: true,
@@ -55,3 +51,5 @@ async function affiche() {
         .render(document.getElementById("pdon"));
 }
 affiche()
+
+
