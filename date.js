d040ff17a1e2b0a297d5fc48c6744bb0740fcd1f
said-ctrl <fifinabs@gmail.com>
2024-07-05 09:06:26 +0200

    upd

--- date.js
+++ date.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
     let datuf = [];
     fetch('./json/datesufc.json').then(res => res.json()).then(data => {
         for (let i = 0; i < data.length; i++) {
-            datuf.push({ title: data[i].Name, start: data[i].DateTime, }
+            datuf.push({ title: data[i].Name, start: data[i].DateTime,
+               
+             }
             )
         }
 
@@ -13,7 +15,13 @@ document.addEventListener('DOMContentLoaded', function () {
         var calendar = new FullCalendar.Calendar(calendarEl, {
 
             initialView: 'dayGridMonth',
-            events: datuf
+            events: datuf,
+            eventTimeFormat: {
+                hour: '2-digit',
+                minute: '2-digit',
+                
+                meridiem: 'long' }
+
         });
 
 
